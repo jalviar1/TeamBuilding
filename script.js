7ba87db1e2361d8a8c2d09b62d17e87a114321a7
Jennifer Alviar <jalviar1@my.hpu.edu.com>
2019-02-26 12:08:01 -1000

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function toRaceCategories() {
+    location.replace("RaceCategories.html")
+  }
+
+function toTheRules() {
+    location.replace("CategoryRules.html")
+  }
+ 
+function toTheMembers() {
+    location.replace("Paddlers.html")
+  }  
+
+function toRegattaQuery() {
+    location.replace("RegattaQuery.html")
+  }  
+
+function toRegattaStacks() {
+    location.replace("RegattaStackList.html")
+  }  
+  
+function toCategoryRules() {
+    location.replace("CategoryRules.html")
+}  
+
+function toRegattaTeams() {
+  location.replace("RegattaStackList.html")
+}  
+
+function toDistanceQuery() {
+  location.replace("DistanceQuery.html")
+} 
+
+function toDistanceTeams() {
+  location.replace("LongDistanceStackList.html")
+}
+
+  
+
+
